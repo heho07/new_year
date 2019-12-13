d2c907ab3abe4ca33c3acdb3f6191c7863f4b79b
Herman <herman.hogman.ording@gmail.com>
2019-12-13 20:14:10 +0100

    Updates

---
+++ precache-manifest.bcb548350fe8c884e1ba48813a08bf90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9feec0395307fb3dea298bffbf6e6bb2",
+    "url": "/new_year/index.html"
+  },
+  {
+    "revision": "9ae45eb05fcccf13cd42",
+    "url": "/new_year/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/new_year/static/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/new_year/static/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ae45eb05fcccf13cd42",
+    "url": "/new_year/static/js/main.a1ba1ede.chunk.js"
+  },
+  {
+    "revision": "a195f03d75c3ebfef1c2",
+    "url": "/new_year/static/js/runtime-main.227bb1df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new_year/static/media/logo.5d5d9eef.svg"
+  }
+]);
